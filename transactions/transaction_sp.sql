b07996d76192109d2f0d1805a0bca2de1354858c
jayzilpilwar <jayzilpilwar@gmail.com>
2023-10-15 23:51:33 +0530

    Code test

--- transactions/transaction_sp.sql
+++ transactions/transaction_sp.sql
@@ -1,4 +1,4 @@
 BEGIN
 create or replace table `dulcet-abacus-397714.transactions.workshop` as
-(select 'Jay' as Name,'27-06-1997' as DOB);
+(select * from dulcet-abacus-397714.transactions.test as c);
 END
