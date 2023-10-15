ab3aa7ae55fa4cb58924206045a1109c8fabdb98
jayzilpilwar <jayzilpilwar@gmail.com>
2023-10-15 23:54:23 +0530

    Code test

--- transactions/transaction_sp.sql
+++ transactions/transaction_sp.sql
@@ -1,4 +1,4 @@
 BEGIN
 create or replace table `dulcet-abacus-397714.transactions.workshop` as
-(select * from dulcet-abacus-397714.transactions.test as c);
+(select * from `dulcet-abacus-397714.transactions.test` as c);
 END
