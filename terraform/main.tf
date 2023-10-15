e4c09b9730d385d19d785da83b2e2834ad091907
Jay <jayzilpilwar@gmail.com>
2023-10-15 18:47:18 +0530

    change add

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,7 +15,6 @@ resource "google_bigquery_dataset" "raw" {
 }
 
 
-
 resource "google_bigquery_routine" "routine" {
     dataset_id      = google_bigquery_dataset.raw.dataset_id
     routine_id      = "${var.routine_id}"
