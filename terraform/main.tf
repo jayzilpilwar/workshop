a91f09d6e5eb7a0690e0051401d4aecf1fd2c8ca
jayzilpilwar <jayzilpilwar@gmail.com>
2023-10-15 23:36:57 +0530

    Code demo

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,6 +13,7 @@ resource "google_bigquery_dataset" "raw" {
         env     = "default"
     }
 }
+
 resource "google_bigquery_routine" "routine" {
     dataset_id      = google_bigquery_dataset.raw.dataset_id
     routine_id      = "${var.routine_id}"
