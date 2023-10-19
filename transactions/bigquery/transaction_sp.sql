e3d0f39f3d5a024b7e6e4b39d528851f26104e99
jayzilpilwar <jayzilpilwar@gmail.com>
2023-10-19 11:49:18 +0530

    code add

--- transactions/bigquery/transaction_sp.sql
+++ transactions/bigquery/transaction_sp.sql
@@ -1,4 +1,1 @@
-BEGIN 
-CREATE OR REPLACE TABLE `dulcet-abacus-397714.transactions.workshop` AS 
-SELECT * FROM `dulcet-abacus-397714.transactions.test`; 
-END
+CREATE OR REPLACE TABLE `dulcet-abacus-397714.transactions.workshop` AS SELECT * FROM `dulcet-abacus-397714.transactions.test`; 
