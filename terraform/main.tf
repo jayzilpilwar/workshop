9b0201f60600912747631aec07b8a6df2865ef0f
jayzilpilwar <jayzilpilwar@gmail.com>
2023-10-19 11:47:48 +0530

    code add

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,6 +20,6 @@ resource "google_bigquery_routine" "bigqyery-execution" {
     routine_id      = "${var.routine_id}"
     routine_type    = "PROCEDURE"
     language        = "SQL"
-    definition_body = file("/bigquery/${var.routine_sql}.sql")
+    definition_body = "/bigquery/${var.routine_sql}.sql"
     depends_on      = [ google_bigquery_dataset.raw ]
 }
