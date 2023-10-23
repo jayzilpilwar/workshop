6bf1ca501b6f936a66f23bdf3a268199493532c4
jayzilpilwar <jayzilpilwar@gmail.com>
2023-10-23 12:15:38 +0530

    main.tf updates

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,32 +19,7 @@
 # }
 
 
-# resource "google_cloudfunctions_function" "my_functions" {
-#   count       = length(var.cloud_functions)
-#   name        = var.cloud_functions[count.index].name
-#   description = var.cloud_functions[count.index].description
-#   runtime     = var.cloud_functions[count.index].runtime
-#   entry_point = var.cloud_functions[count.index].entry_point
-#   source_archive_bucket = var.backend_config
-#   source_archive_object = var.cloud_functions[count.index].source_code
-#   available_memory_mb = 256
-#   timeout             = 60  
-#   region              = "us-east1"
-#   service_account_email = var.service_account_email
-#   event_trigger {
-#     event_type = "google.storage.object.finalize"
-#     resource   = var.backend_config
-#   }
-# }
 
-# resource "google_storage_bucket_object" "function_code" {
-#   count = length(var.cloud_functions)
-#   name   = "${var.cloud_functions[count.index].name}.zip"
-#   bucket = var.backend_config
-#   source  = "cloud_function/${var.cloud_functions[count.index].name}/${var.cloud_functions[count.index].source_code}"
-#   depends_on = [google_cloudfunctions_function.my_functions]
-
-# }
 
 
 resource "google_storage_bucket_object" "archive" {
