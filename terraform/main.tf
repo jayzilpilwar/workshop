faf7d604ac717bd22df7a270dd277b766b872f9c
jayzilpilwar <jayzilpilwar@gmail.com>
2023-10-23 12:09:04 +0530

    main.tf updates

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,22 +1,22 @@
 
-resource "google_bigquery_dataset" "raw" {
-    dataset_id      =   "${var.dataset_id}"
-    friendly_name   =   "${var.dataset_id}"
-    description     =   "This is a raw layer"
-    location        =   "${var.location}"
-    labels  =   {
-        env     = "default"
-    }
-}
-resource "google_bigquery_routine" "bigqyery-execution" {
-    count           = length(var.routine_sql)    
-    dataset_id      = google_bigquery_dataset.raw.dataset_id
-    routine_id      = "routine_${var.routine_id[count.index]}"
-    routine_type    = "PROCEDURE"
-    language        = "SQL"
-    definition_body = file("bigquery/${element(var.routine_sql,count.index)}")
-    depends_on      = [ google_bigquery_dataset.raw ]
-}
+# resource "google_bigquery_dataset" "raw" {
+#     dataset_id      =   "${var.dataset_id}"
+#     friendly_name   =   "${var.dataset_id}"
+#     description     =   "This is a raw layer"
+#     location        =   "${var.location}"
+#     labels  =   {
+#         env     = "default"
+#     }
+# }
+# resource "google_bigquery_routine" "bigqyery-execution" {
+#     count           = length(var.routine_sql)    
+#     dataset_id      = google_bigquery_dataset.raw.dataset_id
+#     routine_id      = "routine_${var.routine_id[count.index]}"
+#     routine_type    = "PROCEDURE"
+#     language        = "SQL"
+#     definition_body = file("bigquery/${element(var.routine_sql,count.index)}")
+#     depends_on      = [ google_bigquery_dataset.raw ]
+# }
 
 
 # resource "google_cloudfunctions_function" "my_functions" {
